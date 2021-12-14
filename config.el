ecc8cb72a761c6e3ee352f861f0ff7dde4491940
Pedro Henrique Romano <mail@pedroromano.org>
2021-12-14 14:46:13 -0300

    Change to dark mode

--- config.el
+++ config.el
@@ -24,7 +24,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-one)
 
 (setq doom-font "Noto Mono-12")
 (setq doom-variable-pitch-font "Noto Sans CJK JP-12")
